5fe28039350c1e128bedafb7084bdace7ba2d3c0
HiiamLongdz <levanlong220700@gmail.com>
2021-11-23 09:44:44 +0700

    update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from asyncio.windows_events import NULL
+import json
 import discord
 from discord.ext import commands
 from colorama import Fore
@@ -5,14 +7,25 @@ import asyncio
 from webserver import keep_alive
 import time
 import re
-
+import requests
+import urllib.parse
 
 import random
 
 from decouple import config
 
-dict = {'⁰': '0', '¹': '1', '²': '2', '³': '3', '⁴': '4',
-        '⁵': '5', '⁶': '6', '⁷': '7', '⁸': '8', '⁹': '9'}
+dict = {
+    '⁰': '0',
+    '¹': '1',
+    '²': '2',
+    '³': '3',
+    '⁴': '4',
+    '⁵': '5',
+    '⁶': '6',
+    '⁷': '7',
+    '⁸': '8',
+    '⁹': '9'
+}
 
 prefix = "q"
 
@@ -48,16 +61,19 @@ async def s(ctx):
             await ctx.send(f"{prefix_owo}b")
             print(f"{at()}: {Fore.GREEN}succefully owob")
 
-        recentMsg = await ctx.bot.get_channel(clientId).history(limit=5).flatten()
+        recentMsg = await ctx.bot.get_channel(clientId).history(limit=5
+                                                                ).flatten()
         for msg in recentMsg:
-            if msg.author.id == 408785106942164992 and 'spent' in msg.content.lower():
+            if msg.author.id == 408785106942164992 and 'spent' in msg.content.lower(
+            ):
                 useGem = random.choice([1, 2])
                 print(f"{Fore.MAGENTA} {useGem}")
                 if useGem == 1:
                     async with ctx.typing():
                         await asyncio.sleep(random.choice([1, 2, 3]))
                         await ctx.send(f"{prefix_owo}inv")
-                    checkInv = await ctx.bot.get_channel(clientId).history(limit=5).flatten()
+                    checkInv = await ctx.bot.get_channel(clientId).history(
+                        limit=5).flatten()
                     msgContent = ''
                     for msgonce in checkInv:
                         if 'inventory' in msgonce.content.lower():
@@ -65,10 +81,11 @@ async def s(ctx):
                     if msgContent:
                         await gem(ctx, msgContent)
 
-            if msg.author.id == 408785106942164992 and 'captcha' in msg.content.lower():
+            if msg.author.id == 408785106942164992 and 'captcha' in msg.content.lower(
+            ):
                 async with ctx.typing():
                     await asyncio.sleep(1)
-                    await ctx.send(f"{prefix}s")
+                    dmcs = False
                     print(f"{at()}: {Fore.RED}stop capcha")
 
         await asyncio.sleep(random.choice([17, 20]))
@@ -99,17 +116,17 @@ async def gem(ctx, msg):
             return 0
 
     for i in range(6):
-        if getValue(str(int(i) + 51)) > 0 and getValue(str(int(i) + 65)) > 0 and getValue(str(int(i) + 72)) > 0:
+        if getValue(str(int(i) + 51)) > 0 and getValue(
+                str(int(i) + 65)) > 0 and getValue(str(int(i) + 72)) > 0:
             gem1 = str(int(i) + 51)
             gem2 = str(int(i) + 65)
             gem3 = str(int(i) + 72)
 
-    for gem in [gem1, gem2, gem3]:
-        async with ctx.typing():
-            await asyncio.sleep(random.choice([7, 8, 9]))
-            await ctx.send(f"{prefix_owo}use {gem}")
-            print(f"{at()}: {Fore.YELLOW}use gem {gem}")
-            await asyncio.sleep(1)
+    async with ctx.typing():
+        await asyncio.sleep(random.choice([7, 8, 9]))
+        await ctx.send(f"{prefix_owo}use {gem1} {gem2} {gem3}")
+        print(f"{at()}: {Fore.YELLOW}use gem {gem1} {gem2} {gem3}")
+        await asyncio.sleep(1)
 
 
 @bot.command()
@@ -119,10 +136,60 @@ async def z(ctx):
     dmcs = False
 
 
+@bot.command(pass_context=True)
+async def count(ctx):
+    await ctx.message.delete()
+
+    channelId = 899852247607943178
+
+    channel = bot.get_channel(channelId)
+
+    global dcount
+    dcount = True
+
+    while dcount:
+        async with channel.typing():
+            await asyncio.sleep(2)
+            message = await channel.history(limit=1).flatten()
+
+            last_msg = message[0]
+
+            if (last_msg.author != bot.user and last_msg.author.bot != True
+                    and len(last_msg.reactions) != 0):
+                print(
+                    f"{at()}: {Fore.GREEN} {last_msg.author.name}: {last_msg.content}"
+                )
+                last_num = await convertNum(last_msg.content.split()[0])
+
+                if (last_num):
+                    await channel.send(last_num + 1)
+                    print(f"{at()}: {Fore.RED}count: {last_num + 1}")
+                    await asyncio.sleep(2)
+        await asyncio.sleep(4)
+
+
+async def convertNum(str):
+    num = NULL
+    try:
+        num = int(str)
+    except:
+        num = NULL
+
+    return num
+
+
+@bot.command()
+async def stop_count(ctx):
+    await ctx.message.delete()
+    global dcount
+    dcount = False
+
+
 @bot.event
 async def on_ready():
     activity = discord.Game(name="", type=4)
     await bot.change_presence(status=discord.Status.online, activity=activity)
     print(f''.join("READY"))
+
 
 bot.run(token, bot=False)
